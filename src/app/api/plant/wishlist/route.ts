c4ce37540929873fd8efb96067c4c87590893c14
julianjauk <jaujuc18@htl-kaindorf.at>
2023-11-02 20:51:01 +0100

    plants: wishlist not finished

---
+++ src/app/api/plant/wishlist/route.ts
@@ -0,0 +1,85 @@
+import { getServerSession } from 'next-auth';
+import prisma from '../../../../../prisma/client';
+import { NextRequest } from 'next/server';
+import { Plant } from '@prisma/client';
+
+export async function GET(request: NextRequest) {
+  try {
+    const session = await getServerSession();
+    const user = await prisma.user.findUnique({
+      where: { email: session?.user?.email },
+      include: {
+        rooms: true,
+      },
+    });
+    const wishlist = await prisma.wishlist.findUnique({
+      where: { userId: user?.id },
+      include: {
+        plants: true,
+      },
+    });
+
+    return wishlist
+      ? new Response(JSON.stringify(wishlist))
+      : new Response('No wishlist', { status: 404 });
+  } catch (e) {
+    return new Response('Unauthenticated' + e, { status: 403 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession();
+    const user = await prisma.user.findUnique({
+      where: { email: session?.user?.email },
+    });
+
+    let wishlist = await prisma.wishlist.findUnique({
+      where: { userId: user?.id },
+      include: {
+        plants: true,
+      },
+    });
+
+    if (wishlist == null) {
+      wishlist = await prisma.wishlist.create({
+        data: {
+          userId: user?.id!,
+        },
+        include: {
+          plants: true,
+        },
+      });
+    }
+
+    const partialPlant = (await request.json()) as Partial<Plant>;
+    delete partialPlant.id;
+    console.log(partialPlant);
+    const plant: Plant = {
+      ...partialPlant,
+    } as Plant;
+    console.log(plant);
+    plant.isOnWishlist = true;
+
+    const updatedWishlist = await prisma.wishlist.update({
+      where: {
+        userId: user?.id!,
+      },
+      data: {
+        plants: {
+          create: plant as Plant,
+        },
+      },
+      include: {
+        plants: true,
+      },
+    });
+
+    return updatedWishlist
+      ? new Response(JSON.stringify(updatedWishlist))
+      : new Response('No wishlist', { status: 404 });
+  } catch (e) {
+    console.error(e);
+    return new Response('Unauthenticated' + e, { status: 403 });
+  }
+}
--- src/components/plant/Plants.tsx
+++ src/components/plant/Plants.tsx
@@ -6,8 +6,10 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getExternalPlants, getExternalPlantsSearch } from '@/app/actions';
 import { ChevronRight } from 'react-feather';
 import { useEffect, useState } from 'react';
-import { Room } from '@prisma/client';
+import { Plant, Room } from '@prisma/client';
 import Searchbar from './Searchbar';
+import axiosClient from '@/utils/axios';
+import camelcaseKeys from 'camelcase-keys';
 
 const getRooms = async (): Promise<Room[]> => {
   const roomResponse = await fetch('http://localhost:3000/api/room');
@@ -46,60 +48,64 @@ export default function Plants({ plants }: { plants: any }) {
       <Searchbar onSearch={handleSearch} />
       <div className='col-span-8 grid grid-cols-10'>
         {data.data &&
-          data.data.map((plantData: any, k: number) => (
-            <Plant
-              key={k.toString(23)}
-              rooms={rooms!}
-              id={plantData.id}
-              common_name={plantData.common_name}
-              image_url={plantData.image_url}
-              scientific_name={plantData.scientific_name}
-            />
-          ))}
+          data.data.map((plantData: any, k: number) => {
+            plantData.name = plantData.common_name;
+            return (
+              <Plant
+                key={k.toString(23)}
+                rooms={rooms!}
+                plant={camelcaseKeys(plantData)}
+              />
+            );
+          })}
       </div>
     </>
   );
 }
+/**
+ *
+ * @param plant
+ */
+const putPlantOnWishlist = async (plant: Partial<Plant>) => {
+  const wishlist = await fetch('http://localhost:3000/api/plant/wishlist', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(plant),
+  });
+};
 
-const Plant = ({
-  rooms,
-  id,
-  common_name,
-  image_url,
-  scientific_name,
-}: {
-  rooms: Room[];
-  id: string;
-  common_name: string;
-  image_url: string;
-  scientific_name: string;
-}) => {
+const Plant = ({ rooms, plant }: { rooms: Room[]; plant: Partial<Plant> }) => {
   return (
     <ContextMenu.Root>
       <ContextMenu.Trigger
         className='flex aspect-auto cursor-pointer flex-col rounded-lg p-3 hover:bg-gray-50'
-        key={id}
+        key={plant.id}
       >
         <div className='relative mb-2 aspect-square w-full rounded-lg bg-primary'>
-          {image_url ? (
+          {plant.imageUrl ? (
             <Image
               fill
               quality={20}
               priority={false}
-              src={image_url}
+              src={plant.imageUrl}
               className='rounded-lg'
-              alt={scientific_name}
+              alt={plant.name!}
             />
           ) : null}
         </div>
         <div className='flex flex-col'>
-          <div className='text-lg font-medium leading-tight'>{common_name}</div>
-          <div className='text-xs text-foreground-grey'>{scientific_name}</div>
+          <div className='text-lg font-medium leading-tight'>{plant.name}</div>
+          <div className='text-xs text-foreground-grey'>{plant.family}</div>
         </div>
       </ContextMenu.Trigger>
       <ContextMenu.Portal>
         <ContextMenu.Content className='min-w-[220px] overflow-hidden rounded-md bg-white p-2 shadow-md'>
-          <ContextMenu.Item className='group relative flex cursor-pointer select-none items-center rounded-md py-1 pl-6 pr-2 text-sm leading-none outline-none transition-colors ease-in hover:bg-primary-light hover:text-primary data-[disabled]:pointer-events-none'>
+          <ContextMenu.Item
+            onClick={() => putPlantOnWishlist(plant)}
+            className='group relative flex cursor-pointer select-none items-center rounded-md py-1 pl-6 pr-2 text-sm leading-none outline-none transition-colors ease-in hover:bg-primary-light hover:text-primary data-[disabled]:pointer-events-none'
+          >
             Add to Wishlist{' '}
             <div className='ml-auto pl-5 group-data-[highlighted]:text-primary'>
               ⌘+N
